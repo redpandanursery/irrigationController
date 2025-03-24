cd5bf61e7c3d59a0238e65f079e279f702ae4231
Thielsen <info@redpandanursery.com>
2025-03-24 08:40:27 -0700

    removing

--- irrigationProgram.py
+++ irrigationProgram.py
@@ -6,7 +6,3 @@ print(x)
 
 systemController = Controller()
 systemController.test()
-
-f = open("/home/redpandanursery/GitHubProjects/irrigationController/stationList.csv","a")
-f.write("new content 5")
-f.close()
