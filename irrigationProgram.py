2ef5d7c61704fcf7d3901e6b20b1aa3c1896632c
redpadanursery <thielsen.lebo@gmail.com>
2025-03-24 08:34:17 -0700

    Added file access class

--- irrigationProgram.py
+++ irrigationProgram.py
@@ -7,6 +7,4 @@ print(x)
 systemController = Controller()
 systemController.test()
 
-f = open("/home/redpandanursery/GitHubProjects/irrigationController/stationList.csv","a")
-f.write("new content 4")
-f.close()
+
