c88652055d2d8ea8b50a24ac538ecce5163300ab
Thielsen <info@redpandanursery.com>
2025-03-23 19:56:07 -0700

    added file write

--- irrigationProgram.py
+++ irrigationProgram.py
@@ -6,3 +6,7 @@ print(x)
 
 systemController = Controller()
 systemController.test()
+
+f = open("/home/redpandanursery/GitHubProjects/irrigationController/stationList.csv","a")
+f.write("new content 4")
+f.close()
