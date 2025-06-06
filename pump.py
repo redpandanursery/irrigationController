8d998c47c5d5759361133bf24530835cc866bb3c
redpadanursery <thielsen.lebo@gmail.com>
2025-06-06 12:14:22 -0700

    added pin testing

--- pump.py
+++ pump.py
@@ -6,6 +6,7 @@ class Pump:
 		self.gpm = 50
 		self.gpmUsage = 0
 		self.board = board
+		self.pinPosition = 0 #for testing pin numbers
 
 	def setUsage(self,gpm):
 		self.gpmUsage = gpm
@@ -15,14 +16,28 @@ class Pump:
 	
 	def getGpmUsage(self):
 		return self.gpmUsage
+	
+	def togglePins(self):
+		pins = [26,19,13,6,5,0,21,20,16,12,1,7,8,25,24,23]
+		if (self.pinPosition > 0):
+			self.board.setPin(pins[self.pinPosition-1],"LOW")
+		self.board.setPin(pins[self.pinPosition],"HIGH")
+		print("Test Pin:")
+		print(pins[self.pinPosition])
+		self.pinPosition = self.pinPosition+1
+
+		
+
 		
 	def turnOn(self,gpmUsage):
-		self.board.setPin(self.pinNumber,"HIGH")
+		self.togglePins()
+		#self.board.setPin(self.pinNumber,"HIGH")
 		self.isOn = True
 		self.gpmUsage = gpmUsage
 		
 	def turnOff(self):
-		self.board.setPin(self.pinNumber,"LOW")
+		self.togglePins()
+		#self.board.setPin(self.pinNumber,"LOW")
 		self.isOn = False
 		self.gpmUsage = 0
 
